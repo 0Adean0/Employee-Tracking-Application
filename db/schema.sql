f43b28c164cee7acfd08b851b6cc8c71a6b46da8
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-27 17:21:35 -0400

    completed schema tables for Department, Title, and Employee, inculded primary key, foreign key and their references

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,20 @@ USE corp_db
 SELECT DATABASE();
 
 CREATE TABLE Department(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (30) NOT NULL);
-
-)
+CREATE TABLE Title(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary INT NOT NULL,
+    region INT NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department (id));
+CREATE TABLE Employees(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name  VARCHAR (30) NOT NULL,
+    title_id INT,
+    FOREIGN KEY (title_id)
+    REFERENCES title(id));

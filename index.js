2480d7de26b91a2d9bda320f0539f8f30f353391
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-27 16:03:52 -0400

    completed first db response for  engageAllDepartments and gave a timeout, will copy base and adjust for the remaining action item choices

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const inquirer = require('inquirer')
 const mysql = requier('mysql12')
 const table = require('console.table')
-const {engageAllDepartments,engageAllTitles,engageAllEmployees,engageAddDepartment,engageAddTitle,engageAddEmployee,engageUpdateEmployee} = require('./queries')
+const { engageAllDepartments, engageAllTitles, engageAllEmployees, engageAddDepartment, engageAddTitle, engageAddEmployee, engageUpdateEmployee } = require('./queries')
 const db = mysql.create({
     host: "localhost",
     user: "root",
-    password:"Imfreezingmynutsoff**",
-    database:"corp_db" 
+    password: "Imfreezingmynutsoff**",
+    database: "corp_db"
 })
 
-const init = () => { inquirer.prompt([{
-    dataType:"list",
-    response:"User's desired action?",
-    name:"action items",
-    items: ["engageAllDepartments,engageAllTitles,engageAllEmployees,engageAddDepartment,engageAddTitle,engageAddEmployee,engageUpdateEmployee"]
+const init = () => {
+    inquirer.prompt([{
+        dataType: "list",
+        response: "User's desired action?",
+        name: "action_items",
+        items: ["engageAllDepartments,engageAllTitles,engageAllEmployees,engageAddDepartment,engageAddTitle,engageAddEmployee,engageUpdateEmployee"]
 
-}])
+    }])
+
+    .then((response) => {
+        const { action_items } = response
+        switch (action_items) {
+            case ("engageAllDepartments"):
+                engageAllDepartments()
+                setTimeout(init, 1000)
+            break
+        }
+    })
 
 }
-init ()
+init()
